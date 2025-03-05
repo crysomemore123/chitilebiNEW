1c227ad1e1637cd8ca404b3f343544ee97bc8e28
crysomemore123 <revaz.gachechiladze.1@btu.edu.ge>
2025-03-05 17:43:34 +0400

    Updated the website for phone

--- app/_components/home-section/Home.tsx
+++ app/_components/home-section/Home.tsx
@@ -2,8 +2,8 @@ import ImageSlider from "../ImageSlider";
 
 function Home() {
   return (
-    <section>
-      <div className="relative md:hidden lg:block min-h-[40rem] h-screen">
+    <section className="overflow-hidden"> {/* Prevents extra white space */}
+      <div className="relative">
         <ImageSlider />
         <h1 className="text-white text-center sm:leading-[5rem] font-bold xss:text-nowrap xss:text-lg xs:text-[1.3rem] sm:text-3xl md:text-4xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <span className="block mb-4">მაღალხარისხიანი ჩითილები</span>
@@ -11,7 +11,7 @@ function Home() {
         </h1>
       </div>
 
-      <div className="xss:mx-5 px-5 sm:mx-10 sm:px-10 md:mx-20 md:px-20 my-14 py-14 shadow-bottom-right flex flex-col gap-5">
+      <div className="xss:mx-5 px-5 sm:mx-10 sm:px-10 md:mx-20 md:px-20 my-8 sm:my-14 py-14 shadow-bottom-right flex flex-col gap-5">
         <h2 className="font-bold text-slate-800 text-lg">
           „ევროპული ჩითილები“ გთავაზობთ ბოსტნეულის და ყვავილების საუკეთესო
           ჩითილებს.
