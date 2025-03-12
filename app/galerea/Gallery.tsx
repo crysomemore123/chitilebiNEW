d2ed61595d388378260586617ea653a586e9d33f
crysomemore123 <revaz.gachechiladze.1@btu.edu.ge>
2025-03-12 15:30:01 +0400

    Updated Gallery.tsx to fix image display issue

--- app/galerea/Gallery.tsx
+++ app/galerea/Gallery.tsx
@@ -1,9 +1,4 @@
-"use client";
-
-import Image from "next/image";
 import { galleryImages, galleryImagesSecond, galleryImagesThird } from "@/app/_utils/constants";
-import Modal from "./Modal";
-import GalleryImageSlider from "./GalleryImageSlider";
 
 function Gallery({ section }: { section: "first" | "second" | "third" }) {
   let imagesToShow = [];
@@ -14,23 +9,8 @@ function Gallery({ section }: { section: "first" | "second" | "third" }) {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full place-items-center my-4">
-      {imagesToShow.map((img, i) => (
-        <Modal key={i}>
-          <Modal.Open opens={`modal-${section}-${i}`}>
-            <div className="relative aspect-square h-[20rem] self-center hover:cursor-pointer">
-              <Image
-                src={img}
-                alt={`Gallery ${i + 1}`}
-                fill
-                sizes="20vw"
-                className="-z-20 w-full object-contain"
-              />
-            </div>
-          </Modal.Open>
-          <Modal.Window name={`modal-${section}-${i}`}>
-            <GalleryImageSlider index={i} />
-          </Modal.Window>
-        </Modal>
+      {imagesToShow.map((image, index) => (
+        <img key={index} src={image.src} alt={`Gallery Image ${index + 1}`} className="w-full h-auto rounded-lg" />
       ))}
     </div>
   );
