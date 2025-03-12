eeac0efd807eed36f77973de8884579005b67d9e
crysomemore123 <revaz.gachechiladze.1@btu.edu.ge>
2025-03-12 16:00:30 +0400

    Fixed on Vercel

--- app/_utils/constants.ts
+++ app/_utils/constants.ts
@@ -66,35 +66,34 @@ import galleryImg63 from "@/public/galerea/63.jpg";
 import galleryImg64 from "@/public/galerea/64.jpg";
 
 export const galleryImages = [
-  galleryImg1, galleryImg2, galleryImg3, galleryImg4, galleryImg5,
-  galleryImg6, galleryImg7, galleryImg8, galleryImg9, galleryImg10,
-  galleryImg11, galleryImg12, galleryImg13, galleryImg14, galleryImg15,
-  galleryImg16, galleryImg17, galleryImg18, galleryImg19, galleryImg20,
-  galleryImg21, galleryImg22, galleryImg23, galleryImg24, galleryImg25,
-  galleryImg26, galleryImg27, galleryImg28, galleryImg29, galleryImg30,
-  galleryImg31, galleryImg32, galleryImg33, galleryImg34, galleryImg35,
-  galleryImg36, galleryImg37,
+  "/galerea/1.png", "/galerea/2.jpg", "/galerea/3.jpg", "/galerea/4.jpg", "/galerea/5.jpg",
+  "/galerea/6.png", "/galerea/7.jpg", "/galerea/8.jpg", "/galerea/9.jpg", "/galerea/10.jpg",
+  "/galerea/11.jpg", "/galerea/12.jpg", "/galerea/13.jpg", "/galerea/14.jpg", "/galerea/15.jpg",
+  "/galerea/16.jpg", "/galerea/17.jpg", "/galerea/18.jpg", "/galerea/19.jpg", "/galerea/20.jpg",
+  "/galerea/21.jpg", "/galerea/22.jpg", "/galerea/23.jpg", "/galerea/24.jpg", "/galerea/25.jpg",
+  "/galerea/26.png", "/galerea/27.jpg", "/galerea/28.png", "/galerea/29.png", "/galerea/30.png",
+  "/galerea/31.jpg", "/galerea/32.jpg", "/galerea/33.jpg", "/galerea/34.jpg", "/galerea/35.jpg",
+  "/galerea/36.jpg", "/galerea/37.jpg"
 ];
 
 export const galleryImagesSecond = [
-  galleryImg38, galleryImg39, galleryImg40, galleryImg41, galleryImg42,
-  galleryImg43, galleryImg44, galleryImg45,
+  "/galerea/38.jpg", "/galerea/39.jpg", "/galerea/40.jpg", "/galerea/41.jpg", "/galerea/42.jpg",
+  "/galerea/43.jpg", "/galerea/44.jpg", "/galerea/45.jpg"
 ];
 
 export const galleryImagesThird = [
-  galleryImg46, galleryImg47, galleryImg48, galleryImg49, galleryImg50,
-  galleryImg51, galleryImg52, galleryImg53, galleryImg54, galleryImg55,
-  galleryImg56, galleryImg57, galleryImg58, galleryImg59, galleryImg60,
-  galleryImg61, galleryImg62, galleryImg63, galleryImg64,
+  "/galerea/46.jpg", "/galerea/47.jpg", "/galerea/48.jpg", "/galerea/49.jpg", "/galerea/50.jpg",
+  "/galerea/51.jpg", "/galerea/52.jpg", "/galerea/53.jpg", "/galerea/54.jpg", "/galerea/55.jpg",
+  "/galerea/56.jpg", "/galerea/57.jpg", "/galerea/58.jpg", "/galerea/59.jpg", "/galerea/60.jpg",
+  "/galerea/61.jpg", "/galerea/62.jpg", "/galerea/63.jpg", "/galerea/64.jpg"
 ];
 
 export const galleryImagesFourth = [
-  "/galerea/66.jpg", "/galerea/67.jpg", "/galerea/68.jpg", "/galerea/69.jpg",
-  "/galerea/70.jpg", "/galerea/71.jpg", "/galerea/72.jpg", "/galerea/73.jpg",
-  "/galerea/74.jpg", "/galerea/75.jpg", "/galerea/76.jpg", "/galerea/77.jpg",
-  "/galerea/78.jpg", "/galerea/79.jpg", "/galerea/80.jpg", "/galerea/81.jpg",
-  "/galerea/82.jpg", "/galerea/83.jpg", "/galerea/84.jpg", "/galerea/85.jpg",
+  "/galerea/66.jpg", "/galerea/67.jpg", "/galerea/68.jpg", "/galerea/69.jpg", "/galerea/70.jpg",
+  "/galerea/71.jpg", "/galerea/72.jpg", "/galerea/73.jpg", "/galerea/74.jpg", "/galerea/75.jpg",
+  "/galerea/76.jpg", "/galerea/77.jpg", "/galerea/78.jpg", "/galerea/79.jpg", "/galerea/80.jpg",
+  "/galerea/81.jpg", "/galerea/82.jpg", "/galerea/83.jpg", "/galerea/84.jpg", "/galerea/85.jpg",
   "/galerea/86.jpg", "/galerea/87.jpg", "/galerea/88.jpg"
 ];
 
-export const galleryVideo = "/galerea/65.mp4"; // Path to your local video
+export const galleryVideo = "/galerea/65.mp4";
