70ed96b2fd6efe1c509952206bb6d5ae3e0c0a9f
crysomemore123 <revaz.gachechiladze.1@btu.edu.ge>
2025-03-12 16:02:00 +0400

    2nd attempt to fix on Vercel

--- app/galerea/Gallery.tsx
+++ app/galerea/Gallery.tsx
@@ -3,27 +3,32 @@ import { galleryImages, galleryImagesSecond, galleryImagesThird, galleryImagesFo
 
 function Gallery({ section }: { section: "first" | "second" | "third" | "fourth" }) {
   let imagesToShow: string[] = [];
+  let videoToShow: string | null = null;
 
   if (section === "first") imagesToShow = galleryImages;
   else if (section === "second") imagesToShow = galleryImagesSecond;
   else if (section === "third") imagesToShow = galleryImagesThird;
-  else if (section === "fourth") imagesToShow = galleryImagesFourth;
+  else if (section === "fourth") {
+    videoToShow = galleryVideo;
+    imagesToShow = galleryImagesFourth;
+  }
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full place-items-center my-4">
-      {section === "fourth" && (
-        <video controls className="w-full h-auto rounded-lg">
-          <source src={galleryVideo} type="video/mp4" />
+      {videoToShow && (
+        <video controls className="w-full rounded-lg">
+          <source src={videoToShow} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       )}
+
       {imagesToShow.map((image, index) => (
         <Image
           key={index}
           src={image}
           alt={`Gallery Image ${index + 1}`}
-          width={500}
-          height={300}
+          width={500} 
+          height={300} 
           className="w-full h-auto rounded-lg"
         />
       ))}
